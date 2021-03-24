957013d7d7ee6c25d358f65b465929ae734ebb49
Francesco <Francesco@DESKTOP-C23CH64.wind3.hub>
2021-03-24 14:26:03 +0100

    Soluzione 1 parte

--- src/main/java/it/polito/tdp/corsi/model/Model.java
+++ src/main/java/it/polito/tdp/corsi/model/Model.java
@@ -1,7 +1,44 @@
 package it.polito.tdp.corsi.model;
 
+import java.util.List;
+import java.util.Map;
+
+import it.polito.tdp.corsi.db.CorsoDAO;
 
 public class Model {
+	
+	private CorsoDAO corsoDao ;
+	
+	
+	public Model() {
+		corsoDao = new CorsoDAO();
+	}
+	
+	
+	
+	public List<Corso> getCorsoByPeriodo(Integer pd){
+		return corsoDao.getCorsoByPeriodo(pd);
+	}
+	
+	
+	public Map<Corso,Integer> getIscrittiiByPeriodo(Integer pd){
+		return corsoDao.getIscrittiByPeriodo(pd);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
